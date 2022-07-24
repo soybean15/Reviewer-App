72419f552ef011cd6a1b087aaf7565d56b25cf3e
soybean15 <marlonpadilla1593@gmail.com>
2022-07-24 20:01:59 +0800

    🦾few updates

--- app/src/main/java/com/devour/reviewerapp/activities/fragments/SearchFragment.kt
+++ app/src/main/java/com/devour/reviewerapp/activities/fragments/SearchFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -49,12 +51,47 @@ class SearchFragment(listener: SearchFragmentListener) : Fragment() {
     fun convertToHashMap(items: MutableList<Item>):HashMap<String,MutableList<String>>{
         val map : HashMap<String,MutableList<String>> = HashMap()
         for (item in items){
-            val descList = item.desc.split("[\\\\s,.?:-]")
+            val descList = item.desc.lowercase().split("[\\\\s,.?:-]")
             map[item.title] = descList.toMutableList()
         }
         return map
 
     }
+    fun findItem(fragment: View){
+        val searchFragmentEditText:EditText = fragment.findViewById(R.id.searchFragmentEditText)
+        val searchFragmentButton:Button = fragment.findViewById(R.id.searchFragmentButton)
+        searchFragmentButton.setOnClickListener {
+            val desc = searchFragmentButton.text.toString().lowercase().split("[\\\\s,.?:-]")
+        }
+
+
+    }
+
+    fun findTopAnswer(desc:List<String>): HashMap<Int, String>{
+        val bestAnswer: HashMap<Int, String> = HashMap()
+
+        for (key in map.keys) {
+            val score: Int = searchProbability(key, desc)
+            if (score != 0) {
+                bestAnswer[score] = key
+            }
+        }
+        return bestAnswer
+    }
+
+   fun searchProbability(key:String, desc:List<String>):Int{
+       var rating = 0
+
+       for(str in desc){
+           if(map[key]!!.contains(str)){
+               rating+=5
+           }
+       }
+       return rating
+
+   }
+
+
 
 
 
